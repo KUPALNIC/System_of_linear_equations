da4ce1b320fdc67a7a9f189ec44fef44a0c0c184
KUPALNIC <sashakupa2411@gmail.com>
2026-02-13 21:32:14 +0300

    first_attemo

---
+++ 1/progonka_method.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class three_matrix(){
+    public:
+    vector<double> c;
+    vector<double> a;
+    vector<double> b;
+    vector<double> d;
+};
+
+
+double equasion(three_matrix A){
+    vector<double> x;
+    vector<double> p(), q();
+    p[0] = -c[0]/b[0];
+    p[0] = d[0]/b[0];
+    for (int i=1; i < A.a.size()-1){  
+    
+    }
+
+}
+
+int main(){
+    return 0;
+}
